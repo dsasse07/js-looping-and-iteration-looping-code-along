81f71e39e6491c7ffb390895137b4541ce6e2c7d
Daniel Sasse <dsasse07@gmail.com>
2021-01-30 16:07:05 -0500

    commit tree dc75c5e2b190154db1aa2587fd4f57eb65ec6d8c

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+
+// function writeCards(names, event) {
+//   let thankyouCards = []
+//   for (i in names) {
+//       thankyouCards.push(`Thank you, ${names[i]}, for the ${event} gift!`)
+//   }
+// }
+
+function writeCards(names, event) {
+  let thankYouCards = []
+
+  for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return thankYouCards
+}
+
+function countDown(num) {
+  let i = num
+  while (i>=0) {
+    console.log(i)
+    i--
+  }
+  
+}
